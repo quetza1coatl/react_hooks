f276101230c662dbc9d7c6a9b3f3e29207d3ab98
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-09-19 19:16:40 +0300

    hooks:useEffect, fetch data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, {Component, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import ReactDOM from "react-dom";
 
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
     if (visible) {
         return (
@@ -13,10 +13,7 @@ const App = () => {
                 <button
                     onClick={() => setVisible(false)}>hide
                 </button>
-                <HookCounter value={value} />
-                <ClassCounter value={value} />
-                <Notification />
-
+                <PlanetInfo id={value} />
             </div>
         );
     } else {
@@ -27,42 +24,46 @@ const App = () => {
     }
 };
 
-const HookCounter = ({ value }) => {
+const PlanetInfo = ({id}) => {
+    const [planetName, setPlanetName] = useState(null)
     useEffect(() => {
-        console.log('useEffect');
-        return () => console.log('clear');  // is used before next useEffect call, incl before component did unmount
-    },
-        [value]);  // useEffect will be called only if value will be changed
-    return <p>{value}</p>
+            let cancelled = false;
+            fetch(`https://swapi.dev/api/planets/${id}`)
+                .then(res => res.json())
+                .then(data => !cancelled && setPlanetName(data.name));
+            return () => cancelled = true;
+        },[id]);
+
+    return <p>{id} - {planetName}</p>
 }
 
-const Notification = () => {
-    const [visible, setVisible] = useState(true);
-    useEffect(() => {
-        const timeout = setTimeout(() => setVisible(false), 2500);
-        return () => clearTimeout(timeout)
-    }, []);
-    return (
-        <div>
-            { visible && <p>Hello</p>}
-        </div>
-    );
-}
+// const Notification = () => {
+//     const [visible, setVisible] = useState(true);
+//     useEffect(() => {
+//         const timeout = setTimeout(() => setVisible(false), 2500);
+//         return () => clearTimeout(timeout)
+//     }, []);
+//     return (
+//         <div>
+//             { visible && <p>Hello</p>}
+//         </div>
+//     );
+// }
 
-class ClassCounter extends Component{
-    componentDidMount() {
-        console.log('class: mount')
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-       console.log('class: update')
-    }
-    componentWillUnmount() {
-        console.log('class: unmount')
-    }
-    render() {
-        return <p>{this.props.value}</p>
-    }
-}
+// class ClassCounter extends Component{
+//     componentDidMount() {
+//         console.log('class: mount')
+//     }
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//        console.log('class: update')
+//     }
+//     componentWillUnmount() {
+//         console.log('class: unmount')
+//     }
+//     render() {
+//         return <p>{this.props.value}</p>
+//     }
+// }
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
